7c9f66c39169041552c529bdde83da4852836950
Kumar Nitin <kumarnitin10081999@gmail.com>
2026-07-28 19:41:41 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,78 @@
+public class Variable {
+
+    public static void main(String[] args) {
+    	/*
+    	
+        // create/declare
+        int age;
+        
+        // Below line of code will give below error
+        // Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+    	// The local variable age may not have been initialized 
+        //System.out.println(age);
+        
+        
+        age = 10;
+        age = 11;
+        System.out.println(age);
+        
+        
+        // define and initialize
+        int totalMarks = 20;
+        System.out.println(totalMarks);
+        
+        
+
+        
+        // Case Sensitive
+        int weight = 80;
+        int WEIGHT = 90;
+        System.out.println("weight:" + weight);
+        System.out.println("WEIGHT:" + WEIGHT);
+        
+        
+
+
+        
+        // Starting Letter
+        int marks = 10;
+        int MARKS = 11;
+        int _marks = 12;
+        int $marks = 13;
+        
+        
+
+        // Subsequent Letters
+        int height5 = 21;
+        int height_love = 33;
+        int height$12Love = 44;
+        int height_ = 22;
+        int height$ = 23;
+        
+        
+
+        // No reserved keyword should be used as variable
+        // int class = 22;
+    	
+    	// main is not a reserved keyword
+        int main = 22;
+        
+        */
+
+        
+        // length
+        int helloJeeKaiseHoSaareMyNameIsKumarNitin = 101;
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        // CamelCase
+        int myNameIsKumarNitin = 51;
+        int totalMarks = 100;
+
+        // Constants
+        int DAYS_IN_YEAR = 365;
+        
+
+    }
+}
