4a7aba170faf840b2fad10b73e331b964708c69f
Kumar Nitin <kumarnitin10081999@gmail.com>
2026-07-28 09:09:37 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,13 @@
+// Code for printing
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello World" + "Hello Dunia");
+        System.out.println(3);
+        System.out.println("4+3");
+        System.out.println(4+3);
+        System.out.print("Kumar Nitin");
+    }
+}
+
+
+
