3ab6a88e470cc8383ea5feb7808eb4caebc8fec3
Kumar Nitin <kumarnitin10081999@gmail.com>
2026-07-29 10:18:26 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,75 @@
+package com.kumarnitin.loops;
+
+public class Loop {
+	public static void main(String[] args) {
+		
+		/*
+		for(int i = 1; i <= 5; i = i + 1) {
+			System.out.println("Value of i: " + i);
+		}
+		
+		
+		for(int i = 1; i <= 10; i = i+1) {
+			System.out.println("Kumar Nitin");
+		}
+		
+		
+		for(int i = 2; i <= 20; i = i + 2) {
+			System.out.println(i);
+		}
+		
+		
+		for(int i = 1; i <= 4; i++) {
+			for(int j = 1; j <= 4; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		
+		for(int i = 1; i <= 3; i++) {
+			for(int j = 1; j <= 3; j++) {
+				System.out.println("i = " + i + ", j = " + j);
+			}
+		}
+		
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i == 10) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i == 1 || i == 2 || i == 3 || i == 4) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		
+		// initialization
+		int i = 1;
+		
+		// condition
+		while (i <= 10) {
+			// process
+			System.out.println("Nitin");
+			// update
+			i++;		
+		}
+		*/
+		
+		// initialization
+		int i = 1;
+	
+		do {
+			System.out.println(i);
+			
+			// Update
+		i++;
+	} while( i <= 10);
+	}
+}
